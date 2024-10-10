3efc9e0b5f4938a4b0122b9896fc7cd200671395
maximmacarone <maximmacarone@gmail.com>
2024-10-10 17:11:03 +0300

    Initial Commit

---
+++ JustConverter/JustConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustConverterApp.swift
+//  JustConverter
+//
+//  Created by Maxim Makarenkov on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct JustConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JustConverterTests/JustConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustConverterTests.swift
+//  JustConverterTests
+//
+//  Created by Maxim Makarenkov on 10.10.2024.
+//
+
+import Testing
+@testable import JustConverter
+
+struct JustConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
